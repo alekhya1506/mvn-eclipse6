69f1456c00b646b74948522dbc857557d37c28a3
alekhya1506 <alekhya15k@gmail.com>
2019-05-23 11:12:10 +0530

    eclipse commit

---
+++ src/test/java/eclipse6/BinaryDecimaltest.java
@@ -0,0 +1,14 @@
+package eclipse6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinaryDecimaltest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
